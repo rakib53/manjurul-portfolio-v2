ec2854539f84aa9d2e562a9884df705b6e8c538b
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-05-25 00:30:26 +0600

    added another slider pointer

--- app/sections/Articles/ArticleSlider.jsx
+++ app/sections/Articles/ArticleSlider.jsx
@@ -13,6 +13,7 @@ import "swiper/css/navigation";
 // import required modules
 import { Pagination, Navigation } from "swiper/modules";
 import ArticleCard from "./ArticleCard";
+import { nextSliderIcon, prevSliderIcon } from "@/utils/SVG";
 
 export default function ArticleSlider() {
   return (
@@ -20,10 +21,17 @@ export default function ArticleSlider() {
       <Swiper
         slidesPerView={3}
         spaceBetween={36}
-        pagination={{
-          clickable: true,
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         }}
-        navigation={true}
+        pagination={{
+          el: ".swiper-pagination",
+          clickable: true,
+          renderBullet: (index, className) => {
+            return `<span class="${className} custom-bullet"></span>`;
+          },
+        }}
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
@@ -48,6 +56,18 @@ export default function ArticleSlider() {
         <SwiperSlide>
           <ArticleCard />
         </SwiperSlide>
+
+        <div className="absolute bottom-[50px] w-full z-50 flex justify-center">
+          <div className="w-[300px] flex justify-center items-center !gap-6">
+            <div className="swiper-button-prev custom-button">
+              {prevSliderIcon}
+            </div>
+            <div className="swiper-pagination !w-fit"></div>
+            <div className="swiper-button-next custom-button">
+              {nextSliderIcon}
+            </div>
+          </div>
+        </div>
       </Swiper>
     </>
   );
