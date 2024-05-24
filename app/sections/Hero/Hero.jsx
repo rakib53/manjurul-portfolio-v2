dcd412b17ff9f97ab02d0ccc119a0273172c686d
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-05-25 00:40:36 +0600

    fixed hero section

--- app/sections/Hero/Hero.jsx
+++ app/sections/Hero/Hero.jsx
@@ -8,8 +8,8 @@ import {
   heroSmallSectionStarIcon,
   orangePlayIcon,
   orangeRightArrow,
-  resumeIcon,
 } from "@/utils/SVG";
+import peoples from "../../../public/peoples.png";
 import Container from "@/app/components/Container";
 
 const HeroSection = () => {
@@ -17,7 +17,7 @@ const HeroSection = () => {
     <section className="bg-[#A6DEF7] py-[110px] flex flex-col items-center text-center">
       <Container>
         <div className="relative flex justify-center items-center mb-8">
-          <span className="absolute top-[50%] left-0">
+          <span className="absolute w-full h-full top-[50%] left-0">
             {heroSectionStarIcon}
           </span>
           <span className="absolute top-[50%] right-0">
@@ -29,33 +29,9 @@ const HeroSection = () => {
           <span className="absolute bottom-0 top-auto left-10">
             {heroSmallSectionStarIcon}
           </span>
-          <div className="relative h-[200px]">
-            <Image
-              src={ClinetOne}
-              alt="Person 1"
-              className="w-16 h-16 rounded-full absolute -top-8 -left-8"
-            />
-            <Image
-              src={ClinetOne}
-              alt="Person 2"
-              className="w-16 h-16 rounded-full absolute top-8 -left-12"
-            />
-            <Image
-              src={ClinetOne}
-              alt="Person 3"
-              className="w-24 h-24 rounded-full absolute bottom-0"
-            />
-            <Image
-              src={ClinetOne}
-              alt="Person 4"
-              className="w-16 h-16 rounded-full absolute top-8 left-12"
-            />
-            <Image
-              src={ClinetOne}
-              alt="Person 5"
-              className="w-16 h-16 rounded-full absolute -top-8 -right-8"
-            />
-          </div>
+        </div>
+        <div className="w-full flex justify-center mb-20">
+          <Image src={peoples} />
         </div>
         <div>
           <h3 className="sectionTitle">
