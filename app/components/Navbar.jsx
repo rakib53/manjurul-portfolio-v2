551d7952e11a93c77ef99b6206b37342051f3685
rakib53 <rakibdev53@gmail.com>
2025-01-05 13:53:59 +0600

    updated some bg and also updated footer

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,51 +1,39 @@
-import React from "react";
-import Container from "./Container";
+import { noteIcon } from "@utils/SVG";
+import Link from "next/link";
 import Button from "./Button";
-import { bangladeshIcon, noteIcon } from "@utils/SVG";
+import Container from "./Container";
 
 export default function Navbar() {
   return (
     <div className="w-full">
       <Container>
         <div className="w-full flex justify-between items-center">
-          <div className="flex flex-col text-gray800 tracking-[0.305px] uppercase font-rocGroteskBold">
-            <span>Manjurul</span>
-            <div className="flex items-center gap-2">
-              <span>Islam</span>
-              <span>{bangladeshIcon}</span>
-            </div>
+          <div className="flex flex-col text-gray800 text-lg leading-7 tracking-[0.305px] font-rocGroteskBold">
+            <span className="">Md Manjurul Islam</span>
           </div>
 
           <ul className="flex items-center gap-[30px]">
-            <li>
-              <a
+            <li className="bg-white py-2 px-4 rounded-md">
+              <Link
                 href="#portfolio"
-                className="text-sm text-gray600 opacity-85 font-rocGroteskMid uppercase tracking-[ 0.56px]"
+                className="text-sm text-gray600 opacity-85 font-rocGroteskMid tracking-[ 0.56px]"
               >
-                Portfolio
-              </a>
+                Home
+              </Link>
             </li>
             <li>
-              <a
-                href="#about-me"
-                className="text-sm text-gray600 opacity-85 font-rocGroteskMid uppercase tracking-[ 0.56px]"
+              <Link
+                href="my-story"
+                className="text-sm text-gray600 opacity-85 font-rocGroteskMid tracking-[ 0.56px]"
               >
-                About me
-              </a>
-            </li>
-            <li>
-              <a
-                href="#blogs"
-                className="text-sm text-gray600 opacity-85 font-rocGroteskMid uppercase tracking-[ 0.56px]"
-              >
-                Blogs
-              </a>
+                My story
+              </Link>
             </li>
           </ul>
 
           <div className="flex items-center">
             <Button
-              label="My resume"
+              label="Contact me"
               link="https://drive.google.com/file/d/19mzhsmOTDttj0kBzWfMW4aHLupHPfGv3/view?usp=sharing"
               linkTarget="_blank"
               className="gray_btn flex items-center justify-between gap-[10px] py-2 px-4 font-rocGroteskMid"
--- app/sections/Partnership/Partnership.jsx
+++ app/sections/Partnership/Partnership.jsx
@@ -1,66 +1,61 @@
 import peoples from "@public/peoples.png";
-import {
-  heroSectionStarIcon,
-  heroSmallSectionStarIcon,
-  orangePlayIcon,
-  orangeRightArrow,
-} from "@utils/SVG";
+import { orangePlayIcon, orangeRightArrow } from "@utils/SVG";
 import Button from "app/components/Button";
-import Container from "app/components/Container";
 import Image from "next/image";
 
 const Partnership = () => {
   return (
-    <section className="py-[100px] flex flex-col items-center text-center partnership">
-      <Container>
-        <div className="w-full relative">
-          <div>
-            <span className="absolute w-full top-[50%] left-0">
-              {heroSectionStarIcon}
-            </span>
-            <span className="absolute top-[50%] right-0">
-              {heroSectionStarIcon}
-            </span>
-            <span className="absolute top-0 right-[100px]">
-              {heroSmallSectionStarIcon}
-            </span>
-            <span className="absolute bottom-[-40px] top-auto left-[200px]">
-              {heroSmallSectionStarIcon}
-            </span>
-          </div>
-          <div className="w-full flex justify-center mb-20">
-            <Image src={peoples} alt="" />
-          </div>
-          <div>
-            <h3 className="sectionTitle">
-              Partnering with Top <br /> Innovators to Transform Ideas into
-              Reality.
-            </h3>
-            <p className="sectionDesc !text-gray700">
-              Explore My Portfolio to Witness My Track Record of Excellence.
-            </p>
-          </div>
-          <div className="flex justify-center space-x-4 pt-[40px]">
-            <Button
-              label="Book a call"
-              link="https://cal.com/manjurul"
-              linkTarget="_blank"
-              className="orange_btn flex items-center gap-[10px] py-2 px-4"
-              icon={orangeRightArrow}
-              iconPosition="right"
-            />
-            <Button
-              label="Show reels"
-              link="https://www.youtube.com/watch?v=Pu_71gLGegk"
-              linkTarget="_blank"
-              labelClass="text-gray800"
-              className="white_btn flex items-center gap-[10px] py-2 px-4"
-              icon={orangePlayIcon}
-              iconPosition="right"
-            />
-          </div>
+    <section className="w-full px-6 flex flex-col items-center text-center">
+      {/* <div className="w-full relative"> */}
+      {/* <div>
+          <span className="absolute w-full top-[50%] left-0">
+            {heroSectionStarIcon}
+          </span>
+          <span className="absolute top-[50%] right-0">
+            {heroSectionStarIcon}
+          </span>
+          <span className="absolute top-0 right-[100px]">
+            {heroSmallSectionStarIcon}
+          </span>
+          <span className="absolute bottom-[-40px] top-auto left-[200px]">
+            {heroSmallSectionStarIcon}
+          </span>
+        </div> */}
+      <div className="partnership py-[120px]">
+        <div className="w-full flex justify-center mb-20">
+          <Image src={peoples} alt="" />
         </div>
-      </Container>
+        <div>
+          <h3 className="sectionTitle">
+            Partnered with leading innovators & drive business growth.
+          </h3>
+          <p className="sectionDesc !text-gray700">
+            Discover how we can transform your vision into impactful
+            solutions—let’s make it happen together!
+          </p>
+        </div>
+        <div className="flex justify-center space-x-4 pt-[40px]">
+          <Button
+            label="Book a call"
+            link="https://cal.com/manjurul"
+            linkTarget="_blank"
+            className="orange_btn flex items-center gap-[10px] py-2 px-4"
+            icon={orangeRightArrow}
+            iconPosition="right"
+          />
+          <Button
+            label="Show reels"
+            link="https://www.youtube.com/watch?v=Pu_71gLGegk"
+            linkTarget="_blank"
+            labelClass="text-gray800"
+            className="white_btn flex items-center gap-[10px] py-2 px-4"
+            icon={orangePlayIcon}
+            iconPosition="right"
+          />
+        </div>
+      </div>
+
+      {/* </div> */}
     </section>
   );
 };
